e98244c0df87d0c3ad5a9d685a89fa4c1a5a026e
iaw21752927 <iaw21752927@h08.informatica.escoladeltreball.org>
2017-09-18 17:51:17 +0200

    first commit

---
+++ src/org/escoladeltreball/helloworld/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.escoladeltreball.helloworld;
+
+/**
+ * @author iaw21752927
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
